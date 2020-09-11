e3ac0bbdb1e13c3c3e1ce16109c74c1dd0fbab76
Vishavjeet Kumar Singh <1131@SSRTECHVISION.LOCAL>
2020-09-11 13:21:06 +0530

    Updates

---
+++ precache-manifest.fbf541dd5c94aad38040c78af3088c2e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c516b52c8bbeed4f8a3d6ed7ecb379",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4ce72c1a34be77c0ff2f",
+    "url": "/portfolio/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "22e171ca94713a181002",
+    "url": "/portfolio/static/css/main.6af062b4.chunk.css"
+  },
+  {
+    "revision": "4ce72c1a34be77c0ff2f",
+    "url": "/portfolio/static/js/2.a00cf97b.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/portfolio/static/js/2.a00cf97b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22e171ca94713a181002",
+    "url": "/portfolio/static/js/main.c653565c.chunk.js"
+  },
+  {
+    "revision": "272c84328c4e9a9d0bda",
+    "url": "/portfolio/static/js/runtime-main.6093c233.js"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/portfolio/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/portfolio/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/portfolio/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/portfolio/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/portfolio/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/portfolio/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/portfolio/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/portfolio/static/media/software.43049cae.jpeg"
+  }
+]);
